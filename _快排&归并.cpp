babdcad533612111511d5af531248f41a3736841
JKERVOID <139824914+JKERVOID@users.noreply.github.com>
2024-08-25 18:40:00 +0800

    Add files via upload
    update 25/8/24

--- _快排&归并.cpp
+++ _快排&归并.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-int n;
 
-//https://www.luogu.com.cn/problem/P1177
+//归并排序可顺带求逆序对
+int n;
 void quick_sort(int q[],int l=1,int r=n){
     if(l>=r) return;
     int i=l-1,j=r+1,x=q[(l+r)>>1];
@@ -18,30 +18,48 @@ void quick_sort(int q[],int l=1,int r=n){
     quick_sort(q,j+1,r);
 }
 
-const int N=1e5+10;
+const int N=5e5+10;
 int tmp[N];
-void merge_sort(int q[],int l=1,int r=n){
-    if(l>=r) return;
+ll merge_sort(int q[],int l=1,int r=n){
+    if(l>=r) return 0;
+
     int mid=(l+r)>>1;
-    merge_sort(q,l,mid);
-    merge_sort(q,mid+1,r);
+    ll res=merge_sort(q,l,mid)+merge_sort(q,mid+1,r);
     int i=l,j=mid+1,k=0;
     while(i<=mid&&j<=r){
         if(q[i]<=q[j]) tmp[k++]=q[i++];
-        else tmp[k++]=q[j++];
+        else{
+            res+=mid-i+1;
+            tmp[k++]=q[j++];
+        } 
     }
     while(i<=mid) tmp[k++]=q[i++];
     while(j<=r) tmp[k++]=q[j++];
 
     for(int i=l,j=0;i<=r;i++,j++) q[i]=tmp[j];
-
+    return res;
 }
-
+/*
 int main(){
+    //https://www.luogu.com.cn/problem/P1177
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
     cin>>n;
-    int q[n+1];
+    int q[N];
     for(int i=1;i<=n;i++) cin>>q[i];
     merge_sort(q);
     for(int i=1;i<=n;i++) cout<<q[i]<<" ";
     return 0;
+}*/
+
+int main(){
+    //https://www.luogu.com.cn/record/174705434
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int q[N];
+        
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>q[i];
+    cout<<merge_sort(q)<<endl;
+    
 }
