566a9c4bce842049300944318a64f82c8b7e5e54
NULL\Viod <sup_eric@163.com>
2025-08-11 16:36:14 +0800

    新增三维向量运算

--- 计算几何/_向量运算.cpp
+++ 计算几何/_向量运算.cpp
@@ -11,33 +11,24 @@ public:
     double x,y;
     P(double _x=0,double _y=0):x(_x),y(_y){};
 
-    std::ostream& operator<<(std::ostream& os){
-        os<<'('<<x<<','<<y<<")";
-        return os;
+    friend std::ostream& operator<<(std::ostream& os, P self){
+        return os<<'('<<self.x<<','<<self.y<<")";
     }
-    P operator+(P _o)const{
-        return P(x+_o.x,y+_o.y);
-    } 
-    P operator-(P _o)const{
-        return P(x-_o.x,y-_o.y);
-    }
-    friend P operator*(double num,P &self){
-        //数乘：此写法只允许左乘数字
-        return P(num*self.x,num*self.y);
-    }
-    double operator*(P _o)const{
-        //叉积：=|a||b|sinθ (平行四边形的有向面积)
-        //o在*this逆时针方向(左侧)为正
-        return x*_o.y-y*_o.x;
-    }
+    
+    P operator+(P _o)const  {return P(x+_o.x,y+_o.y);} 
+    P operator-(P _o)const  {return P(x-_o.x,y-_o.y);}
 
-    friend double dot(P _a, P _b){
-        //点乘：=|a||b|cosθ
-        return _a.x*_b.x+_a.y*_b.y;
-    }
-    double len(){
-        return sqrt(x*x+y*y);
-    }
+    //数乘
+    friend P operator*(double num,P &self)  {return P(num*self.x,num*self.y);}
+
+    /* 叉积：=|a||b|sinθ (平行四边形的有向面积)，o在*this逆时针方向(左侧)为正*/
+    double operator*(P _o)const {return x*_o.y-y*_o.x;}
+    
+    /*点乘：=|a||b|cosθ*/
+    friend double dot(P _a, P _b)   {return _a.x*_b.x+_a.y*_b.y;}
+
+    double len()    {return sqrt(x*x+y*y);}
+
     friend double angle(P a,P b){
         //起始点为a,终点为b
         if(a*b>-eps)
@@ -72,3 +63,35 @@ P getNode(P _a,P _u, P _b,P _v){
     double _t=(_a-_b)*_v/(_v*_u);
     return _a+_t*_u;
 }
+
+
+// class P{
+// public:
+//     double x,y,z;
+//     P(double _x=0, double _y=0, double _z=0):x(_x),y(_y),z(_z){}
+    
+//     friend std::ostream& operator<<(std::ostream& os, P self){
+//         return os<<'('<<self.x<<','<<self.y<<','<<")";
+//     }
+//     P operator+(P _o)const{
+//         return P(x+_o.x,y+_o.y,z+_o.z);
+//     } 
+//     P operator-(P _o)const{
+//         return P(x-_o.x,y-_o.y,z-_o.z);
+//     }
+//     friend P operator*(double num,P &self){
+//         //数乘：此写法只允许左乘数字
+//         return P(num*self.x,num*self.y,num*self.z);
+//     }
+//     P operator*(P _o)const{
+//         //叉积：=|a||b|sinθ (平行四边形的有向面积)
+//         //方向右手定则
+//         return P(y*_o.z-z*_o.y, z*_o.x-x*_o.z, x*_o.y-y*_o.x);
+//     }
+
+//     friend double dot(P _a, P _b){
+//         //点乘：=|a||b|cosθ
+//         return _a.x*_b.x + _a.y*_b.y + _a.z*_b.z;
+//     }
+
+// };
